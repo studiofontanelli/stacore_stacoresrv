1d36d56c04d885ba8600544bb775f95042f8c353
Andrea Fontanelli <andrea1.fontanelli@consulenti.csi.it>
2022-10-26 10:30:04 +0200

    implementato servizio calcolo rimborso

--- docs/swagger/src/main/java/it/csi/stacore/stacoresrv/api/service/RimborsiApi.java
+++ docs/swagger/src/main/java/it/csi/stacore/stacoresrv/api/service/RimborsiApi.java
@@ -10,7 +10,7 @@ import io.swagger.jaxrs.*;
 
 import it.csi.stacore.stacoresrv.api.dto.DatiCalcoloRimborsoVO;
 import it.csi.stacore.stacoresrv.api.dto.ErrorDto;
-import it.csi.stacore.stacoresrv.api.dto.EsitoCalcolaRimborso;
+import it.csi.stacore.stacoresrv.api.dto.EsitoCalcolaRimborsoVO;
 
 import java.util.List;
 import java.util.Map;
@@ -36,13 +36,11 @@ public interface RimborsiApi  {
     @Path("/calcola")
     @Consumes({ "application/json" })
     @Produces({ "application/json" })
-    @io.swagger.annotations.ApiOperation(value = "", notes = "Effettua il calcolo del rimborso ", response = EsitoCalcolaRimborso.class, tags={ "rimborsi", })
+    @io.swagger.annotations.ApiOperation(value = "", notes = "Effettua il calcolo del rimborso ", response = EsitoCalcolaRimborsoVO.class, tags={ "rimborsi", })
     @io.swagger.annotations.ApiResponses(value = { 
-        @io.swagger.annotations.ApiResponse(code = 200, message = "OK", response = EsitoCalcolaRimborso.class),
+        @io.swagger.annotations.ApiResponse(code = 200, message = "OK", response = EsitoCalcolaRimborsoVO.class),
         
         @io.swagger.annotations.ApiResponse(code = 400, message = "Parametro non valorizzato", response = Void.class),
-        
-        @io.swagger.annotations.ApiResponse(code = 404, message = "Comune non trovato", response = Void.class),
         
         @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error", response = ErrorDto.class) })
     public Response calcolaRimborso(@ApiParam(value = "I dati calcolo per effettuare il rimborso " ,required=true) DatiCalcoloRimborsoVO datiCalcolo,@Context SecurityContext securityContext, @Context HttpHeaders httpHeaders );
